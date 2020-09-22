efe6a52d4f5870b102bed84f84ae81aa28fb3023
mmunocan <mmunocan@gmail.com>
2020-09-22 19:06:06 -0300

    Implementacion Boyer Moore

--- text_search.cpp
+++ text_search.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map> 
 #include "text_search.h"
 
 using namespace std;
@@ -29,10 +30,53 @@ int* text_search_bruteforce(string t, string p){
 	return c;
 }
 
-int* text_search_metodo1(string t, string p){
-	int* c = (int *) malloc(sizeof(int));
-	c[0] = 0;
 
+/* Implementación Boyer Moore */
+int* text_search_metodo1(string t, string p){
+	// Mayor cantidad de coincidencias posibles según el tamaño de los strings
+	int max_coin = t.length() - p.length() + 1;
+	// Se reserva la cantidad de elementos + el contador que va a inicio.SEguroa
+	int* c = (int *) malloc(sizeof(int) * (max_coin + 1));
+	c[0] = 0;
+	
+	/* Procesamiento funcion de ultima ocurrencia */
+	map<char, int> lastOcurrence;
+	for(int i = 0; i < t.length(); i++){
+		lastOcurrence[t[i]] = -1;
+	}
+	for(int i = 0; i < p.length(); i++){
+		lastOcurrence[p[i]] = i;
+	}
+	
+	int i = p.length() - 1;
+	int j = p.length() - 1;
+	/* Revisión del patrón en el texto */
+	while(i < t.length()){
+		/* El patron coincide con la revisión actual */
+		if(t[i] == p[j]){
+			/* El patrón coincide completamente. Hay un matching */
+			if(j == 0){
+				c[0]++;
+				c[c[0]] = i;
+				i += p.length();
+				j = p.length() - 1;
+			/* El patrón coincide parcialmente. La revisión debe continuar */
+			}else{
+				i--;
+				j--;
+			}
+		/* El patrón no coincide. Se debe realizar el salto del patrón. */
+		}else{
+			i = i + p.length() - min(j, 1 + lastOcurrence[t[i]]);
+			j = p.length() - 1;
+		}
+	}
+	
+	c = (int *) realloc(c, sizeof(int) * (c[0] + 1));
+	if(c == NULL){
+		printf("Error! en la reubicación de memoria del resultado.\n");
+		return NULL;
+	}
 	return c;
 }
 
